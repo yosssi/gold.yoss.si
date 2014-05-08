c5d225ae6e929b339eb2b14be4c6ac701393f314
yosssi <yoshida.keiji.84@gmail.com>
2014-05-09 02:00:07 +0900

    Add templates.go

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 stylus:
 	stylus ./public/css/style.styl
+bindata:
+	go-bindata -o ./templates.go templates
+build:
+	go build ./...
--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-martini/martini"
@@ -9,8 +10,17 @@ import (
 	"github.com/yosssi/rendergold"
 )
 
+var (
+	stringTemplate = map[string]string{}
+	templateNames  = []string{
+		"layout",
+		"top",
+	}
+)
+
 // main executes main processes.
 func main() {
+	setStringTemplate()
 	m := martini.Classic()
 	m.Use(rendergold.Renderer())
 	m.Get("/", func(r render.Render) {
@@ -20,7 +30,24 @@ func main() {
 			map[string]interface{}{
 				"Title": "Gold - Template engine for Go",
 			},
+			render.HTMLOptions{
+				Layout: "layout" + rendergold.NameContentDelim + stringTemplate["layout"],
+			},
+			render.HTMLOptions{
+				Layout: "top" + rendergold.NameContentDelim + stringTemplate["top"],
+			},
 		)
 	})
 	m.Run()
+}
+
+// setStringTemplate sets string template.
+func setStringTemplate() {
+	for _, name := range templateNames {
+		b, err := Asset(fmt.Sprintf("templates/%s.gold", name))
+		if err != nil {
+			panic(err)
+		}
+		stringTemplate[name] = string(b)
+	}
 }
