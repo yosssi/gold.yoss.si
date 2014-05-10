74a2afcf5dfa6da56cf2905cdfab9385aadb9ea6
yosssi <yoshida.keiji.84@gmail.com>
2014-05-10 17:30:40 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/martini-contrib/render"
@@ -10,19 +9,10 @@ import (
 	"github.com/yosssi/staticbin"
 )
 
-var (
-	stringTemplate = map[string]string{}
-	templateNames  = []string{
-		"layout",
-		"top",
-	}
-)
-
 // main executes main processes.
 func main() {
-	setStringTemplate()
 	m := staticbin.Classic(Asset)
-	m.Use(rendergold.Renderer())
+	m.Use(rendergold.Renderer(rendergold.Options{Asset: Asset}))
 	m.Get("/", func(r render.Render) {
 		r.HTML(
 			http.StatusOK,
@@ -30,24 +20,7 @@ func main() {
 			map[string]interface{}{
 				"Title": "Gold - Template engine for Go",
 			},
-			render.HTMLOptions{
-				Layout: "layout" + rendergold.NameContentDelim + stringTemplate["layout"],
-			},
-			render.HTMLOptions{
-				Layout: "top" + rendergold.NameContentDelim + stringTemplate["top"],
-			},
 		)
 	})
 	m.Run()
-}
-
-// setStringTemplate sets string template.
-func setStringTemplate() {
-	for _, name := range templateNames {
-		b, err := Asset(fmt.Sprintf("templates/%s.gold", name))
-		if err != nil {
-			panic(err)
-		}
-		stringTemplate[name] = string(b)
-	}
 }
